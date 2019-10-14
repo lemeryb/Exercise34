0f468bd626d6eee4f038a312d5cc412fca295217
Benjamin Lemery <lemeryb@hartwick.edu>
2019-10-14 16:12:31 -0400

    Master

---
+++ exercise34.py
@@ -0,0 +1,32 @@
+# Title: Exercise 34
+# Author: Benjamin Lemery
+# Date: 10/14/19
+# This program demonstrates the use of manipulating list values.
+
+animals = ['bear','python3.6','peacock','kangaroo','whale','dolphin']
+
+print("The first animal at 1 is " + animals[0])
+print("The animal at 0 is the 1st animal and is a " + animals[0])
+
+print("The third animal at 3 is a " + animals[2])
+print("The animal at 0 is the 1st animal and is a " + animals[2])
+
+
+print("The second animal (2ns) is a " + animals[1])
+print("The animal at 1 is the 2nd animal and is a " + animals[1])
+
+print("The third animal  is a " + animals[2])
+print("The animal at 3 is the 1st animal and is a " + animals[2])
+
+print("The fourth animal  is a " + animals[4])
+print("The animal at 4 is the fifth  animal and is a " + animals[4])
+
+print("The second animal  is a " + animals[2])
+print("The animal at 2 is the second  animal and is a " + animals[2])
+
+print("The sixth animal  is a " + animals[5])
+print("The animal at 5 is the sixth animal and is a " + animals[5])
+
+
+
+
